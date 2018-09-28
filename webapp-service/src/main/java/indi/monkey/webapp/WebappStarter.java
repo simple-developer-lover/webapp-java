c67332ddfb3ed16eaede5374e1c4774d98847df2
唐剑飞 <wb-tjf399322@alibaba-inc.com>
2018-09-28 10:37:20 +0800

    modify

--- webapp-service/src/main/java/indi/monkey/webapp/WebappStarter.java
+++ webapp-service/src/main/java/indi/monkey/webapp/WebappStarter.java
@@ -3,7 +3,7 @@ package indi.monkey.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class WebappStarter {
 
 	protected static final String BASE_PACKAGES = "indi.monkey.webapp";
